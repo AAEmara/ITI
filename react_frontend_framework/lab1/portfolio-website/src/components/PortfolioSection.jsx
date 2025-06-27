b4e41307025d0eef003abecfd995d7b7ef044927
AAEmara <abdelrahmanemara18@gmail.com>
2025-06-27 20:28:01 +0300

    feat(lab1): update portfolio component.
    Updated the Portfolio component through:
      - Adding Cards items from react-bootstrap library.
      - Added technology icons and images.

--- react_frontend_framework/lab1/portfolio-website/src/components/PortfolioSection.jsx
+++ react_frontend_framework/lab1/portfolio-website/src/components/PortfolioSection.jsx
@@ -1,22 +1,88 @@
-import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import StrategizeImg from "../assets/strategize-red.png";
+import CascadeImg from "../assets/cascade.png";
+import SeceneSmith from "../assets/scenesmith.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faJs,
+  faPython,
+  faNode,
+  faReact,
+  faVuejs,
+} from "@fortawesome/free-brands-svg-icons";
+import FlaskImg from "../assets/flask.png";
 
 const PortfolioSection = () => {
   const projects = [
-    { name: "Cascade", github: "https://github.com/AAEmara/Cascade" },
-    { name: "Strategize", github: "https://github.com/AAEmara/Strategize" },
-    { name: "SceneSmith", github: "https://github.com/AAEmara/SceneSmith" },
+    {
+      name: "Cascade",
+      img: CascadeImg,
+      technologies: [
+        { type: "fontawesome", icon: faJs },
+        { type: "fontawesome", icon: faNode },
+      ],
+      description: "",
+      liveHost: null,
+      github: "https://github.com/AAEmara/Cascade",
+    },
+    {
+      name: "Strategize",
+      img: StrategizeImg,
+      technologies: [
+        { type: "fontawesome", icon: faPython },
+        { type: "image", icon: FlaskImg },
+        { type: "fontawesome", icon: faVuejs },
+      ],
+      description: "",
+      liveHost: "https://aaemara.github.io/Strategize/",
+      github: "https://github.com/AAEmara/Strategize",
+    },
+    {
+      name: "SceneSmith",
+      img: SeceneSmith,
+      technologies: [
+        { type: "fontawesome", icon: faJs },
+        { type: "fontawesome", icon: faReact },
+      ],
+      description: "",
+      liveHost: "https://scene-smith-cw1s.vercel.app/",
+      github: "https://github.com/AAEmara/SceneSmith",
+    },
   ];
 
   return (
     <Container className="py-5">
-      <Row className="g-4">
+      <h1>Portfolio</h1>
+      <Row className="d-flex justify-content-center">
         {projects.map((project) => (
           <Col xs={12} sm={6} md={3} key={project.name}>
-            <Card>
-              <Card.Body className="d-flex align-items-center justify-content-center p-0">
-                {project.name}
-              </Card.Body>
+            <Card className="my-3">
+              <Card.Img variant="top" src={project.img} />
+              <Card.Title className="pt-1 ps-1">{project.name}</Card.Title>
+              <Card.Text>{project.description}</Card.Text>
+              <div className="d-flex justify-content-center">
+                {project.technologies.map((technology, idx) => {
+                  if (technology.type === "fontawesome") {
+                    return (
+                      <FontAwesomeIcon
+                        className="me-2"
+                        icon={technology.icon}
+                        size="2x"
+                        key={idx}
+                      />
+                    );
+                  } else if (technology.type === "image") {
+                    return (
+                      <img
+                        className="me-2"
+                        src={technology.icon}
+                        style={{ width: 32, height: 32 }}
+                        key={idx}
+                      />
+                    );
+                  }
+                })}
+              </div>
             </Card>
           </Col>
         ))}
